2604b906fc2d1862c9afd355f9a5b402b4773672
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-01 16:20:59 -0700

    policy demo

---
+++ Demo03.ParameterizedPolicy/ParameterizedPolicy.ps1
@@ -0,0 +1,23 @@
+﻿$rg = New-AzureRmResourceGroup -Name policy -Location WestEurope
+
+$locationPolicyDef = Get-AzureRmPolicyDefinition -Id /providers/Microsoft.Authorization/policyDefinitions/e56962a6-4747-49cd-b67b-bf8b01975c4c
+
+$locationPolicyDef.Properties.parameters
+
+$locationPolicyDef.Properties.policyRule.if
+
+$locationPolicyDef.Properties.policyRule.then
+
+$locationsArray = @("northeurope", "westeurope")
+
+$policyParameters = @{"listOfAllowedLocations"=$locationsArray}
+
+New-AzureRMPolicyAssignment -Name locationAssignment -Scope $rg.ResourceId -PolicyDefinition $locationPolicyDef -PolicyParameterObject $policyParameters
+
+$policyAssignment = Get-AzureRmPolicyAssignment -Scope $rg.ResourceId
+
+$policyAssignment.Properties.parameters.listOfAllowedLocations
+
+New-AzureRmStorageAccount -Name ryjoneswillfail -Location westus -ResourceGroupName policy -SkuName Standard_LRS
+
+New-AzureRmStorageAccount -Name ryjoneswillsucceed -Location westeurope  -ResourceGroupName policy -SkuName Standard_LRS
