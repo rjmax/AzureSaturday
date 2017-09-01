5d05f3008702c927767f98c0fbce2edf996c0cfc
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-01 10:37:34 -0700

    AKV samples

---
+++ Demo01.KeyVault/KeyVault.ps1
@@ -0,0 +1,7 @@
+﻿New-AzureRmResourceGroup -Location WestEurope -Name keyVault -Force
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName keyVault -TemplateUri https://raw.githubusercontent.com/rjmax/AzureSaturday/master/Demo01.KeyVault/KeyVaultSetup.json
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName keyVault -TemplateUri https://raw.githubusercontent.com/rjmax/AzureSaturday/master/Demo01.KeyVault/KeyVaultUse.json
+
+Remove-AzureRmResourceGroup -Name keyVault -Force
