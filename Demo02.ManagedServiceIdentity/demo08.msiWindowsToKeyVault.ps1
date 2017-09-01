dbe3c9678a53aca633905eafd1b7b2f8366ef25d
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-01 15:42:18 -0700

    Windows MSI script

---
+++ Demo02.ManagedServiceIdentity/demo08.msiWindowsToKeyVault.ps1
@@ -0,0 +1,9 @@
+﻿New-AzureRmResourceGroup -Name msiWindows -Location westus -Force
+
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName msiWindows -TemplateUri https://raw.githubusercontent.com/rjmax/AzureSaturday/master/Demo02.ManagedServiceIdentity/demo08.msiWindowsToKeyvault.json
+
+$deployment
+
+$deployment.Outputs.PowerShellCommandToGetKeyVaultToken.Value
+
+Remove-AzureRmResourceGroup -Name msiWindows
