c98731f346a83007398d7c6c9db8849e2372c999
Ryan Jones (AZURE RESOURCE MANAGER) <ryjones@microsoft.com>
2017-09-01 11:17:00 -0700

    linux MSI demo script

---
+++ Demo02.ManagedServiceIdentity/demo07.msiLinuxToArm.ps1
@@ -0,0 +1,9 @@
+﻿New-AzureRmResourceGroup -Name msiLinux -Location WestEurope -Force
+
+$deployment = New-AzureRmResourceGroupDeployment -ResourceGroupName msiLinux -TemplateUri https://raw.githubusercontent.com/rjmax/AzureSaturday/master/Demo02.ManagedServiceIdentity/demo07.msiLinuxToArm.json
+
+$deployment
+
+$deployment.Outputs.commandToGetAToken.Value
+
+Remove-AzureRmResourceGroup -Name msiLinux
